de52d56c4c95892fdd11b98ae727c831013d86be
byte <oper_xanax@mail.ru>
2024-06-02 04:41:39 +0300

    9

--- client/Screens/Login/Login.jsx
+++ client/Screens/Login/Login.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Login() {
 
 	const postData = async (id, login) => {
-		await fetch('http://109.71.247.72:3001/user', {
+		await fetch('https://okpool.ru/user', {
 				method: 'POST',
 				headers: {
 						'Content-Type': 'application/json',
@@ -14,7 +14,7 @@ export default function Login() {
 	}
 	
 	const getData = async () => {
-		const response = await fetch('http://109.71.247.72:3001/user')
+		const response = await fetch('https://okpool.ru/user')
 		const data = await response.json()
 		console.log(data)
 	}
--- client/Screens/Preloader/Preloader.jsx
+++ client/Screens/Preloader/Preloader.jsx
@@ -9,7 +9,7 @@ export default function Preloader() {
     useEffect(() => {
         const fetchData = async (userId) => {
             try {
-                const response = await fetch(`http://109.71.247.72:3001/user?id=${userId}`);
+                const response = await fetch(`https://okpool.ru/user?id=${userId}`);
                 if (response.ok) {
                         setFoundId(true);
                 } else {
